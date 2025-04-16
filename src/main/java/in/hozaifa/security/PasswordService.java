db4c4abd969a0a8116f65c4933da5d9f0fce017e
Md-Hozaifa <abumonzir2020@gmail.com>
2025-04-17 00:31:57 +0530

    first commit

---
+++ src/main/java/in/hozaifa/security/PasswordService.java
@@ -0,0 +1,35 @@
+package in.hozaifa.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+
+	public static String encoder(String txt) {
+		
+		Encoder encoder = Base64.getEncoder();
+		String encodeToString = encoder.encodeToString(txt.getBytes());
+		return encodeToString;
+	}
+	
+	public static String decoder(String decodeTxt) {
+		
+		Decoder decoder = Base64.getDecoder();
+		byte[] decode = decoder.decode(decodeTxt);
+		
+		
+		return new String(decode);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		String encoder = PasswordService.encoder("hozaifa@123");
+		System.out.println(encoder);
+		
+		String decoder = PasswordService.decoder(encoder);
+		System.out.println(decoder);
+	}
+	
+}
